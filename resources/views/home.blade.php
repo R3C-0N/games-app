312e8bd9e48fe824b22081b17ac336cdad290df4
Mathis Medard <mathismedard.74@gmail.com>
2021-04-25 15:07:38 +0200

    Home page and css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.template')
+
+@section('title', 'Home')
+
+@section('sidebar')
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection

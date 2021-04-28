1f97030bbf2bb82c3e43967e2df7af33ba4eb6ce
Mathis Medard <mathismedard.74@gmail.com>
2021-04-29 00:26:10 +0200

    Darkmode Front

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,25 +1,64 @@
 <script src="{{asset('js/nav.js')}}"></script>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-{{ isset(Auth::user()->darkmode) && Auth::user()->darkmode ? 'dark' : 'light' }}">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Games</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <h2 class="navbar-brand">
+            {{ config('app.name', 'Laravel') }}
+        </h2>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="nav nav-pills me-auto mb-2 mb-lg-0">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav nav-pills mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/connect">Se connecter</a>
+                    <a class="nav-link" aria-current="page" href="/">{{ __('Accueil') }}</a>
                 </li>
             </ul>
-            <form class="d-flex" action="/search" method="post">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-primary disabled" type="submit">Search</button>
-            </form>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="nav nav-pills ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{ route('login') }}">{{ __('Se connecter') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{ route('register') }}">{{ __('S\'enregistrer') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Deconnexion') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                            <li><a class="dropdown-item" href="#">{{ __('Réglages') }}</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li>
+                                <a class="form-check form-switch dropdown-item text-center">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">{{ __('Darkmode') }}</label>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
+            </ul>
         </div>
     </div>
 </nav>
-<div style="margin-bottom: 5vh;"></div>

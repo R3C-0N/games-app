a93af66af195bcb56279c0929103bebc1b3d41fe
Mathis Medard <mathismedard.74@gmail.com>
2021-05-01 17:09:56 +0200

    Error correction, just ignore

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="{{ isset(Auth::user()->darkmode) && Auth::user()->darkmode ? 'bg-black' : '' }}">
 <head>
     <title>{{ config('app.name', 'Laravel') }}</title>
 

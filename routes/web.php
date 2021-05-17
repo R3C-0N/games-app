d0a30bd8910c58487a998aec0386d3181d9a1892
Mathis Medard <mathismedard.74@gmail.com>
2021-05-17 20:56:33 +0200

    Working darkmode in back

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\GamesController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
